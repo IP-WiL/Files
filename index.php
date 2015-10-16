7dd062a441723f4e5a287a9317e8c1222106f780
mydavids <yusiry.davids@gmail.com>
2015-10-16 11:03:38 +0200

    Featured Products
    Added tables for featured products

--- index.php
+++ index.php
@@ -1,5 +1,39 @@
 <?php
 
-echo "User landing page";
+$strFeatured = 
+   "<table>
+      <tr>
+         <th>Featured Item 1</th>
+         <th>Featured Item 2</th>
+         <th>Featured Item 3</th>
+      </tr>
+      <tr>
+         <td>Item Image</td>
+         <td>Item Image</td>
+         <td>Item Image</td>
+      </tr>
+      <tr>
+         <td>Item Caption</td>
+         <td>Item Caption</td>
+         <td>Item Caption</td>
+      </tr>
+      <tr>
+         <th>Featured Item 4</th>
+         <th>Featured Item 5</th>
+         <th>Featured Item 6</th>
+      </tr>
+      <tr>
+         <td>Item Image</td>
+         <td>Item Image</td>
+         <td>Item Image</td>
+      </tr>
+      <tr>
+         <td>Item Caption</td>
+         <td>Item Caption</td>
+         <td>Item Caption</td>
+      </tr>
+   </table>";
+
+echo $strFeatured;
 
 ?>
